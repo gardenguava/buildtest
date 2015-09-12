3359a6a4b4065bdc943a7b06f7228b439d915bf8
LV <sample@test.com>
2015-09-11 21:25:31 -0400

    Initial Code

---
+++ Balance Calculator/src/com/build/code/BalanceCalculator.java
@@ -0,0 +1,7 @@
+package com.build.code;
+
+public class BalanceCalculator {
+	public static void main(String[] args) {
+		System.out.println("Balance Calculator");
+	}
+}
---
+++ Task Handler/src/com/build/code/TaskHandler.java
@@ -0,0 +1,10 @@
+package com.build.code;
+
+public class TaskHandler {
+
+	public static void main(String[] args) {
+		System.out.println("Task Handler");
+
+	}
+
+}
